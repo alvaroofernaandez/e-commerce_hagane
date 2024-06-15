ce5257a3d80b503c216a566f1a2133e2591eaf84
Alvaro <alvaroofernaandez@gmail.com>
2024-06-15 17:52:02 +0200

    Header top created and imported

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import HeaderTop from "./components/HeaderTop";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <HeaderTop/>
+        {children}
+      </body>
     </html>
   );
 }
