0e56a1dde5145917591158ca57d8d1f0acaef644
Alvaro <alvaroofernaandez@gmail.com>
2024-06-16 04:38:39 +0200

    HeaderMain added and style modifications

--- src/app/components/HeaderTop.js
+++ src/app/components/HeaderTop.js
@@ -5,7 +5,7 @@ import { VscColorMode } from "react-icons/vsc";
 
 const HeaderTop = () => {
     return (
-        <div className="border-b border-gray-200 hidden sm:block">
+        <div className="border-b border-gray-700 hidden sm:block">
             <div className="container py-4">
                 <div className="flex justify-between items-center">
                     {/* Este es la parte izquierda del Header, donde ubico los botones que redirigen a las RRSS */}
@@ -21,11 +21,11 @@ const HeaderTop = () => {
                         </div>
                     </div>
                     {/* Este es la parte central del Header, donde simplemente añadire una frase*/}
-                    <div className="text-gray-500 text-md[14px]">
+                    <div className="text-white text-md[14px]">
                         <strong>THIS E-COMERCE WEBSITE is a template designed by @alvaroofernandez</strong>
                     </div>
                     {/* Este es la parte derecha del Header, donde pondré un botón para poner el modo oscuro */}
-                    <div className="header_top_icon_wrapper2">
+                    <div className="header_top_icon_wrapper">
                         <div>
                             <VscColorMode />
                         </div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import HeaderTop from "./components/HeaderTop";
+import HeaderMain from "./components/HeaderMain";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <HeaderTop/>
+        <HeaderMain/>
         {children}
       </body>
     </html>
