e01b305d487d9cd477be7c4aa93413dd71661239
Alvaro <alvaroofernaandez@gmail.com>
2024-07-21 20:28:10 +0200

    elements and products added and styled

--- src/app/components/NewProducts.js
+++ src/app/components/NewProducts.js
@@ -1,21 +1,23 @@
 import React from 'react';
+import ProductCard from './ProductCard';
+
 const productsData = [
     {
         img:"/tshirt.png",
         title:"Oversize T-shirt",
         desc:"MEN Oversize T-shirt",
-        rating: 4,
+        rating: 3,
         price: "19.99",
     },
     {
         img:"/baggy-jeans.png",
         title:"Baggy Jeans",
         desc:"MEN Baggy and Comfort Jeans",
-        rating: 5,
+        rating: 4,
         price: "30.99",
     },
     {
-        img:"/shoes-1.png",
+        img:"/shoes_1.png",
         title:"Sneakers",
         desc:"MEN 'OSIRIS' Sneakers",
         rating: 5,
@@ -26,10 +28,10 @@ const productsData = [
 const NewProducts = () => {
     return (
         <div>
-            <div>
-                <h2>New Products</h2>
+            <div className="container pt-16">
+                <h2 className="font-medium text-2xl pb-4 text-blue-300">New Products</h2>
 
-                <div>
+                <div className="grid gridl-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 xl:gap-x-20 xl:gap-y-10">
                     {
                         productsData.map((item, index) => {
                             return (
--- src/app/components/ProductCard.js
+++ src/app/components/ProductCard.js
@@ -1,9 +1,86 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
-const ProductCard = ({img, title, desc, rating, price}) => {
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+
+const ProductCard = ({ img, title, desc, rating, price }) => {
+    const generateRating = (rating) => {
+        switch (rating) {
+            case 1:
+                return (
+                    <div className="flex gap-1 text-[20px] text-blue-300">
+                        <AiFillStar />
+                        <AiOutlineStar />
+                        <AiOutlineStar />
+                        <AiOutlineStar />
+                        <AiOutlineStar />
+                    </div>
+                );
+            case 2:
+                return (
+                    <div className="flex gap-1 text-[20px] text-blue-300">
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiOutlineStar />
+                        <AiOutlineStar />
+                        <AiOutlineStar />
+                    </div>
+                );
+            case 3:
+                return (
+                    <div className="flex gap-1 text-[20px] text-blue-300">
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiOutlineStar />
+                        <AiOutlineStar />
+                    </div>
+                );
+            case 4:
+                return (
+                    <div className="flex gap-1 text-[20px] text-blue-300">
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiOutlineStar />
+                    </div>
+                );
+            case 5:
+                return (
+                    <div className="flex gap-1 text-[20px] text-blue-300">
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                    </div>
+                );
+            default:
+                return null;
+        }
+    };
+
     return (
-        <div>ProductCard</div>
-    )
-}
+        <div className="px-4 border border-gray-700 rounded-xl max-w-[400px]">
+            <div>
+                <Image className="w-full h-auto" src={img} width={200} height={300} alt={title} />
+            </div>
 
-export default ProductCard
+            <div className="space-y-2 py-2 text-white">
+                <h2 className="text-white font-medium uppercase">{title}</h2>
+                <p className="text-gray-500 max-w-[150px]">{desc}</p>
+                <div>{generateRating(rating)}</div>
+
+                <div className="font-bold flex gap-4">
+                    ${price}
+                    <del className="text-gray-500 font-normal">
+                        ${parseInt(price) + 50}.00
+                    </del>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
